2d7da1ec7939589080d33fe8ec5d3e0f58df9f83
jeeshant <jeeshant94@gmail.com>
2023-03-09 14:38:09 -0500

    Initial commit

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log("hello");
+var HTTP_PORT = process.env.port || 8080;
+var express = require("express");
+var app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello from conestoga!");
+});
+app.listen(HTTP_PORT);
